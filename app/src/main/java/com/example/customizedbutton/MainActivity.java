9c927638a61e0299d524e13a9e4e7e96963a4521
ssc-bando <satoshi_bando@solver.co.jp>
2020-03-18 10:11:59 +0900

    add: visivility change

--- app/src/main/java/com/example/customizedbutton/MainActivity.java
+++ app/src/main/java/com/example/customizedbutton/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.customizedbutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button TargetButtonNormal;
+    private FloatingActionButton TragetButtonFloating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TargetButtonNormal = (Button)findViewById(R.id.button);
+    }
+
+    void log(String s){
+        Log.d("TAG", s);
+    }
+
+    public void onVisibilityChangeButtonClicked(View view) {
+        int cur = TargetButtonNormal.getVisibility();
+        int next = View.VISIBLE;
+        switch(cur){
+            case View.VISIBLE:
+                next = View.INVISIBLE;
+                break;
+            case View.INVISIBLE:
+                next = View.GONE;
+                break;
+            case View.GONE:
+                next = View.VISIBLE;
+                break;
+        }
+        TargetButtonNormal.setVisibility(next);
     }
 }
